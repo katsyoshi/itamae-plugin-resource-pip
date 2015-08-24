0d6534f7a6c1ac86c74ff1363739b2148b74a0a7
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-08-24 23:01:15 +0900

    create pip resource

--- lib/itamae/plugin/resource/pip.rb
+++ lib/itamae/plugin/resource/pip.rb
@@ -1,10 +1,22 @@
-require "itamae/plugin/resource/pip/version"
+require 'itamae/resource/base'
 
 module Itamae
   module Plugin
     module Resource
-      module Pip
-        # Your code goes here...
+      class Pip < Itamae::Resource::Base
+        define_attribute :action, default: :install
+        define_attribute :pip_binary, type: [String, Array], default: 'pip'
+        define_attribute :package_name, type: String, default_name: true
+        define_attribute :version, type: String, default: false
+
+        def pre_action
+          case @current_action
+          when :install
+            attributes.installed = true
+          when :uninstall
+            attributes.installed = false
+          end
+        end
       end
     end
   end
