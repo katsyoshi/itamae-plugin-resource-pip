f0b0ebc477f03c173cd223dcf9d0443d1a1865cb
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-08-24 23:18:54 +0900

    add install action

--- lib/itamae/plugin/resource/pip.rb
+++ lib/itamae/plugin/resource/pip.rb
@@ -28,6 +28,18 @@ module Itamae
           end
         end
 
+        def action_install(action_options)
+          if current.installed
+            if attributes.version && current.version != attributes.version
+              install!
+              updated!
+            end
+          else
+            install!
+            updated!
+          end
+        end
+
         private
 
         def installed_pips
@@ -39,6 +51,20 @@ module Itamae
           pips
         rescue Backend::CommandExecutionError
           []
+        end
+
+        def build_pip_install_command
+          cmd = [*Array(attributes.pip_binary), 'install']
+          if attributes.version
+            cmd << "#{attributes.package_name}==#{attributes.version}"
+          else
+            cmd << attributes.package_name
+          end
+          cmd
+        end
+
+        def install!
+          run_command(build_pip_install_command)
         end
       end
     end
