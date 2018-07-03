da0441049be4144e7bccac89d253c069e1417e66
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-07-03 12:01:21 +0900

    change docker image to debian

--- spec/itamae-plugin-resource-pip/pip_spec.rb
+++ spec/itamae-plugin-resource-pip/pip_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
-describe file("/.pip/lib/python3.6/site-packages/tornado") do
+describe file("/.pip/lib/python3.5/site-packages/tornado") do
   it { should be_directory }
 end
 
-describe file("/.pip/lib/python3.6/site-packages/tornado-4.1-py3.6.egg-info") do
+describe file("/.pip/lib/python3.5/site-packages/tornado-4.1.dist-info") do
   it { should be_directory }
 end
