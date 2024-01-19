b41545878c8d5d783682af64628b9c81e67882a6
gemmaro <gemmaro.dev@gmail.com>
2024-01-20 00:21:50 +0900

    Add failing test

--- spec/itamae/plugin/resource/pip_spec.rb
+++ spec/itamae/plugin/resource/pip_spec.rb
@@ -2,13 +2,19 @@ require "spec_helper"
 
 describe Itamae::Plugin::Resource::Pip do
   describe "installed pips" do
-    it "name and version" do
-      output = double("output", stdout: "Babel==2.10.3")
-      backend = double("backend", run_command: output)
-      runner = double("runner", backend: backend)
-      recipe = double("recipe", runner: runner)
-      resource = Itamae::Plugin::Resource::Pip.new(recipe, "test-service")
-      expect(resource.send(:installed_pips)).to eq [{ name: "Babel", version: "2.10.3" }]
+    let(:backend) { double("backend", run_command: output) }
+    let(:runner) { double("runner", backend: backend) }
+    let(:recipe) { double("recipe", runner: runner) }
+    let(:resource) { Itamae::Plugin::Resource::Pip.new(recipe, "test-service") }
+
+    context "with name and version" do
+      let(:output) { double("output", stdout: "Babel==2.10.3") }
+      it { expect(resource.send(:installed_pips)).to eq [{ name: "Babel", version: "2.10.3" }] }
+    end
+
+    context "with name and file" do
+      let(:output) { double("output", stdout: "importlib-metadata @ file:///tmp/guix-build-python-importlib-metadata-5.2.0.drv-0/importlib_metadata-5.2.0/dist/importlib_metadata-5.2.0-py3-none-any.whl") }
+      it { expect(resource.send(:installed_pips)).to eq [{ name: "importlib-metadata" }] }
     end
   end
 end
